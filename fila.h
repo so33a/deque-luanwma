4f631cffd5f7d4d2c88a64d5acccf4a1101b4a87
luanwma <luanwma@hotmail.com>
2016-09-14 21:05:15 -0300

    Update fila.h

--- fila.h
+++ fila.h
@@ -15,6 +15,11 @@ typedef struct {
 link novoNo(int item, link next);
 FILA novaFila();
 void inserir(FILA f, int e);
+void inserir_inicio(FILA f, int x);
+void inserir_fim(FILA f , int x);
+void remover(FILA f);
+void remover_inicio(FILA f);
+void remover_fim(FILA f);
 int remover(FILA f);
 void imprimirFila(FILA f);
 void destroiFila(FILA f);
